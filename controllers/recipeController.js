05ddf14400a14f9f6751d460466f1c69e9a4f257
Jennifer <jenniferpeterson@pursuit.org>
2024-07-10 14:42:35 -0400

    added a post request in order to create recipes and add into the database

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -2,8 +2,9 @@ const express = require("express");
 
 const recipes = express.Router();
 
-const { getAllPublicRecipes } = require("../queries/recipes");
+const { getAllPublicRecipes, createRecipe } = require("../queries/recipes");
 
+// To get ALL PUBLIC recipes
 recipes.get("/", async (req, res) => {
   try {
     const allPublicRecipes = await getAllPublicRecipes();
@@ -11,6 +12,19 @@ recipes.get("/", async (req, res) => {
     res.status(200).json(allPublicRecipes);
   } catch (error) {
     res.status(500).json({ error: "server error" });
+  }
+});
+
+// To create a NEW RECIPE
+recipes.post("/", async (req, res) => {
+  console.log("body", req.body);
+
+  try {
+    const newRecipe = await createRecipe(req.body);
+    console.log("new recipe:", newRecipe);
+    res.status(200).json(newRecipe);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
 });
 
